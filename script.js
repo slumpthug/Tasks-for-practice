baa679d79bbad38630e52dd19a00b8b1c0b7f051
slumpthug <gangguchi58@gmail.com>
2022-07-16 16:51:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+//Задание 1
+// const arr = [true, false, true, false, true];
+
+// const result = arr.filter(item => item == true);
+
+// console.log(result.length);
+
+//Задание 2
+// const user = "sahh";
+
+// const user2 = "stone";
+
+// function play(user, user2) {
+//     if (user === "kamen" && user2 === "paper") {
+//         console.log("первый выйграл");
+//     } else if (user2 === "stone" && user === "paper"){
+//         console.log("второй выйграл");
+//     } else if (user === "scissors" && user2 === "paper"){
+//         console.log("первый выйграл");
+//     } else if (user2 === "scissors" && user === "paper"){
+//         console.log("второй выйграл");
+//     } else if (user === "scissors" && user2 === "stone"){
+//         console.log("второй выйграл");
+//     } else if (user2 === "scissors" && user === "stone"){
+//         console.log("первый выйграл");
+//     } else if (user2 === "paper" && user === "paper"){
+//         console.log("ничья");
+//     } else if (user === "scissors" && user2 === "spapertone"){
+//         console.log("ничья");
+//     } else if (user2 === "stone" && user === "stone"){
+//         console.log("ничья");
+//     } else {
+//         console.log("неправильно ввели значения");
+//     }
+// };
+
+// play(user, user2);
+
+//Задание 3
+//Собвственный твиттер
